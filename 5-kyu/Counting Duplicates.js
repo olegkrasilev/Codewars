8da070d8636599f4d05e90d3dbb40468e058ad1b
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2021-11-15 18:49:14 +0300

    solved Counting Duplicates

---
+++ 5-kyu/Counting Duplicates.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
+const duplicateCount = text => {
+	let count = 0;
+	const stringToArray = text.toLowerCase().split('');
+	const removeDuplicates = new Set(stringToArray);
+	if (removeDuplicates.size === stringToArray.length) {
+		return 0;
+	}
+	removeDuplicates.forEach(item => {
+		const strRegEx = `[${item}]`;
+		const newRegEx = new RegExp(strRegEx, 'gm');
+		const numberOfDuplicate = text.toLowerCase().match(newRegEx);
+		if (numberOfDuplicate.length > 1) {
+			count += 1;
+		}
+	});
+	return count;
+};

5fcdecd1373cb3e92cf0c64967eae80bec80c180
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2021-12-01 17:11:23 +0300

    converted if/else to ternary operator.

--- 5-kyu/Calculating with Functions.js
+++ 5-kyu/Calculating with Functions.js
@@ -6,7 +6,7 @@ const getResult = string => {
 		return +result[0] + +result[2];
 	case '-':
 		return +result[0] - +result[2];
-  	case '*':
+			case '*':
 		return +result[0] * +result[2];
 	case '/':
 		return Math.floor(+result[0] / +result[2]);
@@ -14,64 +14,34 @@ const getResult = string => {
 };
 
 const zero = (result, number = 0) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const one = (result, number = 1) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const two = (result, number = 2) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const three = (result, number = 3) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const four = (result, number = 4) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const five = (result, number = 5) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const six = (result, number = 6) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const seven = (result, number = 7) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const eight = (result, number = 8) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 const nine = (result, number = 9) => {
-	if (!result) {
-		return `${number}`;
-	}
-	return getResult(`${number}${result}`);
+	return !result ? `${number}` : getResult(`${number}${result}`);
 };
 
 const plus = inputNum => `+${inputNum}`;

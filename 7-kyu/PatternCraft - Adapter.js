79c551edee2b29780dd46a646d65f0a2b9af6241
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-05-04 12:54:43 +0300

    solved PatternCraft - Adapter

---
+++ 7-kyu/PatternCraft - Adapter.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56919e637b2b971492000036
+
+class MarioAdapter {
+
+  constructor(mario) {
+    this.mario = mario;
+  }
+
+  attack(target) {
+    this.mario.jumpAttack();
+    target.health -= 3;
+  }
+}

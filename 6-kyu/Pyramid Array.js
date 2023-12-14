fee19995a222721b4d7374c70702c12e3d107c38
Oleg Krasilev <65223358+olegkrasilev@users.noreply.github.com>
2023-12-14 20:06:02 +0300

    solved Pyramid Array

---
+++ 6-kyu/Pyramid Array.js
@@ -0,0 +1,18 @@
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+// Note: the subarrays should be filled with 1s
+
+function pyramid(n) {
+  const result = [];
+
+  for (let i = 1; n >= i; i++) {
+    result.push(Array(i).fill(1));
+  }
+
+  return result;
+}
